0dbeb5d24ebd035b973e0065c8cdd94bd2d9691d
DebdipWritesCode <debdipmukherjee52@gmail.com>
2025-07-19 09:24:39 +0530

    update team member schema

--- src/modules/admin/team-members/team-member.schema.ts
+++ src/modules/admin/team-members/team-member.schema.ts
@@ -3,10 +3,14 @@ import { Document } from 'mongoose';
 
 export type TeamMemberDocument = TeamMember & Document;
 
+export type TeamMemberTypeEnum = 'super' | 'head' | 'manager';
+
 @Schema()
 export class TeamMember {
   @Prop({ required: true }) name: string;
   @Prop({ required: true }) position: string;
+  @Prop({ required: true, enum: ['super', 'head', 'manager'] })
+  type: TeamMemberTypeEnum;
   @Prop({ type: Buffer }) image: Buffer;
   @Prop() imageMimeType: string;
 }
